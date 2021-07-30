b1c33b68ad6112b85a096a2c47243eb5bf2013a7
kemirandad <kjmd2s@gmail.com>
2021-07-30 16:17:25 -0500

    Persistiendo la información scrapeada

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import logging
 import argparse
+import datetime
+import csv 
 
 logging.basicConfig(level=logging.INFO)
 
@@ -33,10 +35,26 @@ def _news_scraper(news_site_uid):
         if article:
             logger.info('Article fetched !!')
             articles.append(article)
-            print(article.title)
+            break
+    
+    _save_articles(news_site_uid, articles)
+
+def _save_articles(news_site_uid, articles):
+    now = datetime.datetime.now().strftime('%Y_%m_%d')
+    out_file_name = '{news_site_uid}_{datetime}_articles.csv'.format(
+        news_site_uid=news_site_uid,
+        datetime=now
+    )
+    csv_headers = list(filter(lambda property: not property.startswith('_'),
+                         dir(articles[0])))
+    
+    with open(out_file_name, mode='w+', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_headers)
         
-    print(len(articles))
-        
+        for article in articles:
+            row = [str(getattr(article, prop))for prop in csv_headers]
+            writer.writerow(row)
         
 def _fetch_article(news_site_uid, host, link):
     logger.info('Start fetching article at {}'.format(link))
