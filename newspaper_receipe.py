94a2e74040c6e0f8da4ced2ee9dbb33249e18331
kemirandad <kjmd2s@gmail.com>
2021-07-31 17:01:25 -0500

    Removiendo caracteres basura de nuestra columna body

--- newspaper_receipe.py
+++ newspaper_receipe.py
@@ -4,6 +4,7 @@ logging.basicConfig(level=logging.INFO)
 
 from urllib.parse import urlparse
 import pandas as pd
+import hashlib
 
 logger = logging.getLogger(__name__)
 
@@ -17,6 +18,8 @@ def main(filename):
 
     df = _extract_host(df)
     df = _fill_missing_titles(df)
+    df = _generate_uids_for_rows(df)
+    df = _remove_new_line_from_body(df)
 
     return df
 
@@ -62,6 +65,31 @@ def _fill_missing_titles(df):
     
     df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:, 'missing_titles']
 
+    return df
+
+def _generate_uids_for_rows(df):
+    logger.info('Generating uids for each row')
+    uids = (df
+            .apply(lambda row: hashlib.md5(bytes(row['url'].encode())), axis=1)
+            .apply(lambda hash_object: hash_object.hexdigest())
+            )
+    df['uid'] = uids
+
+    return df.set_index('uid')
+
+def _remove_new_line_from_body(df):
+    logger.info('Remove new lines from body')
+    stripped_body = (df
+                        .apply(lambda row: row['body'], axis=1)
+                        .apply(str)
+                        .apply(lambda body: list(body))
+                        .apply(lambda letters: list(map(lambda letter: letter.replace('\n', ' ')
+                                                                                .replace('\r', ' '), letters)))
+                        .apply(lambda letters: ''.join(letters))
+                    )
+    
+    df['body'] = stripped_body
+    
     return df
 
 
